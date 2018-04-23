51f470f42bd0cb6678568ff324cb668b5e90e503
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2018-04-23 17:25:50 -0300

    #12 first accessibility enhancement

--- WebContent/src/sound-chat/sound-chat.js
+++ WebContent/src/sound-chat/sound-chat.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-class SoundChat extends CooperativeEditorSound {
+class SoundChat extends SoundChatLocalization {
 	
 	static get is() {
 		return 'sound-chat'; 
