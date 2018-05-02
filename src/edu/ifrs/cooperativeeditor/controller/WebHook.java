025fe76120e0421cb75ceb3f10996cd741aacaec
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2018-05-02 18:12:59 -0300

    #19

--- src/edu/ifrs/cooperativeeditor/controller/WebHook.java
+++ src/edu/ifrs/cooperativeeditor/controller/WebHook.java
@@ -1,6 +1,5 @@
 package edu.ifrs.cooperativeeditor.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -17,14 +16,13 @@ public class WebHook extends HttpServlet {
 	private static final Logger log = Logger.getLogger(WebHook.class.getName());
 	
 	private static final String SCRIPT = "updateEditor.sh";
-	private static final String DIRECTORY = "/home/rodrigo/";
+	private static final String DIRECTORY = "/rodrigo/home/";
 	
 	private void execute() {
 		log.info("Executing Cooperative Editor update");
-		ProcessBuilder pb = new ProcessBuilder(SCRIPT);
-		pb.directory(new File(DIRECTORY));
+		
 		try {
-			pb.start();
+			Runtime.getRuntime().exec(DIRECTORY + SCRIPT);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
