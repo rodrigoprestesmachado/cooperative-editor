3af3b0198487a44a16c39ace508561568318e36a
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2018-05-22 17:40:50 -0300

    Merge

--- WebContent/src/ce-editor/ce-editor.js
+++ WebContent/src/ce-editor/ce-editor.js
@@ -1,3 +1,19 @@
+/**
+ * @license
+ * Copyright 2018,Instituto Federal do Rio Grande do Sul (IFRS)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 class CooperativeEditor extends CooperativeEditorLocalization {
 	static get is() { return 'ce-editor'; }
 	constructor() {
@@ -79,9 +95,12 @@ class CooperativeEditor extends CooperativeEditorLocalization {
      */     
 	_diff(){
 		var text1 = "";
+		var text2 = "";
+		
 		if(this.currentContribution > 0)
 			text1 = this.contributions[this.currentContribution - 1].content;
-		var text2 = this.contributions[this.currentContribution].content;
+		
+		text2 = this.contributions[this.currentContribution].content;
 		var clazz = this._getClassUser(this.contributions[this.currentContribution].user.id);
 		var dmp = new diff_match_patch();
 	   	var d = dmp.diff_main(text1, text2, false);
@@ -112,7 +131,7 @@ class CooperativeEditor extends CooperativeEditorLocalization {
      _setContribution(contribution){
 	   	 this.contributions.push(contribution);
 	   	 this.currentContribution = this.contributions.length - 1;
-    	 this._updateContent(this.contributions[this.currentContribution].content);
+	   	 this._updateContent(this.contributions[this.currentContribution].content);
      }
      
      /**
