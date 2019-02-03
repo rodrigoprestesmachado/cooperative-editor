8951cb2f37ce9929e586228c6cc2d6ceea4e8407
rodrigo <rodrigo@machados-mbp-2.home>
2019-02-03 20:43:44 -0200

    #43

--- src/META-INF/sql/sound-effect.sql
+++ src/META-INF/sql/sound-effect.sql
@@ -1,3 +1,10 @@
-INSERT IGNORE INTO sound_effect (id, effect, color, position) VALUES (1,"NOCOLOR","yellow", "RIGHT"),(2,"DELAY","blue", "LEFT"),(3,"WAHWAH","purple", "RIGHT"),(4,"MOOG","orange", "LEFT");
+INSERT IGNORE INTO sound_effect (id, effect, color, position) VALUES 
+(1,"NOCOLOR","YELLOW", "RIGHT"),(2,"NOCOLOR","LIME", "RIGHT"),
+(3,"DELAY","NAVY", "LEFT"),
+(4,"DELAY","AQUA", "LEFT"),
+(5,"WAHWAH","FUCHSIA", "RIGHT"),
+(6,"WAHWAH","PURPLE", "RIGHT"),
+(7,"MOOG","OLIVE", "RIGHT"),
+(8,"MOOG","SILVER", "LEFT");
 
 INSERT IGNORE INTO user (id, email, name, password) VALUES (1, "rodrigo@teste.com", "Rodrigo", "1"),(2, "lauro@teste.com", "Lauro", "1"),(3, "marcio@teste.com", "Marcio", "1"),(4, "junior@teste.com", "Junior", "1"),(5, "larissa@teste.com", "Larissa", "1"),(6, "luis@teste.com", "Luis", "1");
