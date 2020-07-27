6eb19815a4b1408c6f7b09c3411796c6227bc10f
Rodrigo <rodrigo.prestes@gmail.dom>
2020-07-27 18:19:57 -0300

    fix

--- src/META-INF/sql/sounds.sql
+++
@@ -1,8 +0,0 @@
-INSERT IGNORE INTO sound_effect (id, effect, color, position) VALUES 
-(1,"NOCOLOR","YELLOW", "RIGHT"),(2,"NOCOLOR","LIME", "RIGHT"),
-(3,"DELAY","NAVY", "LEFT"),
-(4,"DELAY","AQUA", "LEFT"),
-(5,"WAHWAH","FUCHSIA", "RIGHT"),
-(6,"WAHWAH","PURPLE", "RIGHT"),
-(7,"MOOG","OLIVE", "RIGHT"),
-(8,"MOOG","SILVER", "LEFT");
--- src/META-INF/sql/users.sql
+++
@@ -1,1 +0,0 @@
-INSERT IGNORE INTO user (id, email, name, password) VALUES (1, "rodrigo@teste.com", "Rodrigo", "1"),(2, "lauro@teste.com", "Lauro", "1"),(3, "marcio@teste.com", "Marcio", "1"),(4, "junior@teste.com", "Junior", "1"),(5, "larissa@teste.com", "Larissa", "1"),(6, "luis@teste.com", "Luis", "1");
