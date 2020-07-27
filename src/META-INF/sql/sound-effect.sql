bab57b2cc4c4a051c8f26fe7cb154f845a77d129
Rodrigo <rodrigo.prestes@gmail.dom>
2020-07-27 17:43:41 -0300

    docker compose fix

--- src/META-INF/sql/sound-effect.sql
+++ src/META-INF/sql/sound-effect.sql
@@ -6,5 +6,3 @@ INSERT IGNORE INTO sound_effect (id, effect, color, position) VALUES
 (6,"WAHWAH","PURPLE", "RIGHT"),
 (7,"MOOG","OLIVE", "RIGHT"),
 (8,"MOOG","SILVER", "LEFT");
-
-INSERT IGNORE INTO user (id, email, name, password) VALUES (1, "rodrigo@teste.com", "Rodrigo", "1"),(2, "lauro@teste.com", "Lauro", "1"),(3, "marcio@teste.com", "Marcio", "1"),(4, "junior@teste.com", "Junior", "1"),(5, "larissa@teste.com", "Larissa", "1"),(6, "luis@teste.com", "Luis", "1");
