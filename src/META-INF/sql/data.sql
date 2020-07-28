da9f532f9aade782146d94771efc0d0fd0ae77eb
Rodrigo <rodrigo.prestes@gmail.dom>
2020-07-28 12:36:52 -0300

    sql fix

--- src/META-INF/sql/data.sql
+++ src/META-INF/sql/data.sql
@@ -1,10 +1,13 @@
-INSERT IGNORE INTO sound_effect (id, effect, color, position) VALUES 
-(1,"NOCOLOR","YELLOW", "RIGHT"),(2,"NOCOLOR","LIME", "RIGHT"),
-(3,"DELAY","NAVY", "LEFT"),
-(4,"DELAY","AQUA", "LEFT"),
-(5,"WAHWAH","FUCHSIA", "RIGHT"),
-(6,"WAHWAH","PURPLE", "RIGHT"),
-(7,"MOOG","OLIVE", "RIGHT"),
-(8,"MOOG","SILVER", "LEFT");
+INSERT INTO `sound_effect` (`id`, `color`, `effect`, `position`) VALUES
+(1, 'YELLOW', 'NOCOLOR', 'RIGHT'),
+(2, 'LIME', 'NOCOLOR', 'RIGHT'),
+(3, 'NAVY', 'DELAY', 'LEFT'),
+(4, 'AQUA', 'DELAY', 'LEFT'),
+(5, 'FUCHSIA', 'WAHWAH', 'RIGHT'),
+(6, 'PURPLE', 'WAHWAH', 'RIGHT'),
+(7, 'OLIVE', 'MOOG', 'RIGHT'),
+(8, 'SILVER', 'MOOG', 'LEFT');
 
-INSERT IGNORE INTO user (id, email, name, password) VALUES (1, "rodrigo@teste.com", "Rodrigo", "1"),(2, "lauro@teste.com", "Lauro", "1"),(3, "marcio@teste.com", "Marcio", "1"),(4, "junior@teste.com", "Junior", "1"),(5, "larissa@teste.com", "Larissa", "1"),(6, "luis@teste.com", "Luis", "1");
+INSERT INTO `user` (`id`, `email`, `name`, `password`) VALUES
+(1, 'rodrigo@teste.com', 'Rodrigo', '1'),
+(2, 'lauro@teste.com', 'Lauro', '1');
